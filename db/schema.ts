9207344f5a16bfa3e11ce23b61ccaced70bf4927
Sanjib Poon <sanjibpoon123@gmail.com>
2025-02-14 23:26:58 +0545

    i don't know changes

--- db/schema.ts
+++ db/schema.ts
@@ -6,10 +6,18 @@ import {
     uuid,
     varchar,
     primaryKey,
-    PgArray
+    PgArray,
+    AnyPgColumn,
+    pgEnum,
+    serial,
+    jsonb,
+    boolean,
 } from "drizzle-orm/pg-core"
 
-// Next-auth's drizzle adapter schema starts here ---------
+import { relations } from 'drizzle-orm';
+
+
+// Next-auth's schema STARTs here ---------
 import type { AdapterAccount } from "next-auth/adapters"
 
 export const users = pgTable("user", {
@@ -20,29 +28,27 @@ export const users = pgTable("user", {
     image: text("image"),
 })
 
-export const accounts = pgTable(
-    "account",
-    {
-        userId: text("userId")
-            .notNull()
-            .references(() => users.id, { onDelete: "cascade" }),
-        type: text("type").$type<AdapterAccount["type"]>().notNull(),
-        provider: text("provider").notNull(),
-        providerAccountId: text("providerAccountId").notNull(),
-        refresh_token: text("refresh_token"),
-        access_token: text("access_token"),
-        expires_at: integer("expires_at"),
-        token_type: text("token_type"),
-        scope: text("scope"),
-        id_token: text("id_token"),
-        session_state: text("session_state"),
-    },
+export const accounts = pgTable("account", {
+    userId: text("userId")
+        .notNull()
+        .references(() => users.id, { onDelete: "cascade" }),
+    type: text("type").$type<AdapterAccount["type"]>().notNull(),
+    provider: text("provider").notNull(),
+    providerAccountId: text("providerAccountId").notNull(),
+    refresh_token: text("refresh_token"),
+    access_token: text("access_token"),
+    expires_at: integer("expires_at"),
+    token_type: text("token_type"),
+    scope: text("scope"),
+    id_token: text("id_token"),
+    session_state: text("session_state"),
+},
     (account) => ({
         compoundKey: primaryKey({
             columns: [account.provider, account.providerAccountId],
         }),
     })
-)
+);
 
 export const sessions = pgTable("session", {
     sessionToken: text("sessionToken").notNull().primaryKey(),
@@ -50,7 +56,7 @@ export const sessions = pgTable("session", {
         .notNull()
         .references(() => users.id, { onDelete: "cascade" }),
     expires: timestamp("expires", { mode: "date" }).notNull(),
-})
+});
 
 export const verificationTokens = pgTable(
     "verificationToken",
@@ -62,20 +68,51 @@ export const verificationTokens = pgTable(
     (vt) => ({
         compoundKey: primaryKey({ columns: [vt.identifier, vt.token] }),
     })
-)
-// and ends here -------------------
+);
+//  next-auth's schema ENDs here -------------------
 
-export const topics = pgTable('topics', {
-    id: uuid('id').primaryKey().notNull(),
-    name: varchar('name').notNull(),
+
+export const TopicsTable = pgTable('topics', {
+    id: uuid('id').primaryKey().defaultRandom(),
+    name: text('name').notNull(),
+    description: text('description'),
+    latestContentDeltaId: uuid('latest_content_delta_id').references(() => ContentDeltasTable.id),
+    createdBy: uuid('created_by').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull()
 });
 
-export const topicRelationships = pgTable('topic_relationships', {
-    childID: uuid('child_id').references(() => topics.id).notNull(),
-    parentID: uuid('parent_id').references(() => topics.id).notNull(),
-}, (table) => {
-    // Add composite primary key to ensure uniqueness of the relationship
-    return {
-        pk: primaryKey({ columns: [table.parentID, table.childID] }),
-    };
+export const ContentDeltasTable = pgTable('content_deltas', {
+    id: uuid('id').primaryKey().defaultRandom(),
+    contentDelta: jsonb('content_delta').notNull(),
+    isFullSnapshot: boolean('is_full_snapshot').notNull(),
+    previousContentDeltaId: uuid('previous_content_delta_id').references((): AnyPgColumn => ContentDeltasTable.id),
+    commitId: uuid('commit_id').references(() => CommitsTable.id).notNull(),
+    createdBy: uuid('created_by').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull()
+});
+
+export const TopicRelationsTable = pgTable('topic_relations', {
+    id: uuid('id').primaryKey().defaultRandom(),
+    parentTopicId: uuid('parent_topic_id').references(() => TopicsTable.id),
+    siblingTopicIds: uuid('sibling_topic_ids').array().notNull(),
+    createdBy: uuid('created_by').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull()
+});
+
+export const CommitsTable = pgTable('commits', {
+    id: uuid('id').primaryKey().defaultRandom(),
+    topicRelationIds: uuid('topic_relation_ids').array().notNull(),
+    commitMessage: text('commit_message').notNull(),
+    previousCommitId: uuid('previous_commit_id').references((): AnyPgColumn => CommitsTable.id),
+    createdBy: uuid('created_by').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull()
+});
+
+export const BranchesTable = pgTable('branches', {
+    id: uuid('id').primaryKey().defaultRandom(),
+    name: text('name').notNull(),
+    topicId: uuid('topic_id').references(() => TopicsTable.id).notNull(),
+    latestCommitId: uuid('latest_commit_id').references(() => CommitsTable.id).notNull(),
+    createdBy: uuid('created_by').notNull(),
+    createdAt: timestamp('created_at').defaultNow().notNull()
 });
