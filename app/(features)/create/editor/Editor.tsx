e1d94fe3d6edbcda1d7f94c21cdc7698b49f41c5
Sanjib Poon <sanjibpoon123@gmail.com>
2025-03-19 06:33:22 +0545

    Replace prepopulated rich text with sample lexical state and update image properties in JSON

--- app/(features)/create/editor/Editor.tsx
+++ app/(features)/create/editor/Editor.tsx
@@ -34,6 +34,8 @@ import EditorStateOnChangePlugin from './plugins/EditorStateOnChangePlugin';
 import { SerializedEditorState, SerializedLexicalNode } from 'lexical';
 import Reader from '../../learn/components/Reader';
 
+import lexicalStateSample from '../lib/lexical-state-sample.json';
+
 export default function Editor() {
 
     const [editorState, setEditorState] = useState<SerializedEditorState<SerializedLexicalNode> | undefined>();
@@ -143,69 +145,11 @@ export default function Editor() {
 
 
 const initialConfig = {
-    editorState: $prepopulatedRichText,
+    editorState: JSON.stringify(lexicalStateSample),
     namespace: "LessonEditor",
     theme,
     nodes: [...defaultEditorNodes],
     onError: (error: any) => {
         console.error(error);
     },
-}
-
-
-function $prepopulatedRichText() {
-    const root = $getRoot();
-    if (root.getFirstChild() === null) {
-        const heading = $createHeadingNode('h1');
-        heading.append($createTextNode('Crafting a Good Lesson'));
-        root.append(heading);
-
-        const paragraph = $createParagraphNode();
-        paragraph.append(
-            $createTextNode('A good lesson often starts with a captivating introduction. The introduction is where you '),
-            $createTextNode('convince learners the ').toggleFormat('italic'),
-            $createTextNode('importance ').toggleFormat('bold').toggleFormat('italic'),
-            $createTextNode("of the lesson.")
-        );
-        root.append(paragraph);
-
-        const heading2 = $createHeadingNode('h2');
-        heading2.append($createTextNode('You will learn'));
-        root.append(heading2);
-
-        const list = $createListNode('bullet');
-        list.append(
-            $createListItemNode().append(
-                $createTextNode('Why and how to structure a lesson')
-            ),
-            $createListItemNode().append(
-                $createTextNode("Why to use different content types")
-            ),
-            $createListItemNode().append(
-                $createTextNode("How to make a lesson engaging")
-            ),
-        );
-        root.append(list);
-
-        const heading3 = $createHeadingNode('h2');
-        heading3.append($createTextNode('Lesson Structure'));
-        root.append(heading3);
-
-        const paragraph2 = $createParagraphNode();
-        paragraph2.append(
-            $createTextNode("Structuring lesson well can make a difference between a book that is pleasant to read and re-read, and one that is hard to follow along. A good lesson has simple structure that is consistent with other lessons. A good lesson can have a following structure:"),
-        );
-        root.append(paragraph2);
-
-        const list2 = $createListNode("number")
-        list2.append(
-            $createListItemNode().append($createTextNode("Introduction")),
-            $createListItemNode().append($createTextNode("Objective (clear & testable)")),
-            $createListItemNode().append($createTextNode("Main Content")),
-            $createListItemNode().append($createTextNode("Summary")),
-            $createListItemNode().append($createTextNode("Exercises"))
-        );
-        root.append(list2);
-
-    }
 }
